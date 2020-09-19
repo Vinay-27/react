058746cf96cf515f0b67fe09472b3a01d8faa653
Vinay-27 <jamalpurvinay2@gmail.com>
2020-09-19 03:48:22 -0600

    Updates

---
+++ precache-manifest.5fb5e9b51cf1660b50c2cc2c50b97cb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af94f539dcaa68d1757f72f78564af73",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "e7cfb95cec2b27ac00e8",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc0d07f6f4b7cce3e971",
+    "url": "/react/static/js/2.f1484533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.f1484533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7cfb95cec2b27ac00e8",
+    "url": "/react/static/js/main.44639fce.chunk.js"
+  },
+  {
+    "revision": "6eaa0b60f820913c77b0",
+    "url": "/react/static/js/runtime-main.a123f851.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
